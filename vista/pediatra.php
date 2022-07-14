fc26291970c618897d702e72bbf6bbb4b4cc79b0
ashleyTE <20183321@aloe.ulima.edu.pe>
2022-07-14 10:51:13 -0500

    Create pediatra.php

---
+++ vista/pediatra.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../recursos/css/pediatra.css">
+    <title>Centro de Vacunación</title>
+</head>
+<body>
+    <section>
+        <div class="tit">
+            <h1>Histoial de Pediatras</h1>
+        </div>
+        <div class="pedia">
+            <h3>Pediatra 1</h3>
+            <div class="inf">
+                <div class="ima">
+                    <img src="../recursos/imagenes/pediatra1.jpg" alt="">
+                </div>
+                <div class="infor">
+                    <p>Nombre y apellido</p>
+                    <p>CMP:</p>
+                    <p>Lugar de atención</p>
+                    <p>Fecha de última atención</p>
+                </div>
+                <div class="infor">
+                    <p>Ratzel Medina Pasos</p>
+                    <p>763549</p>
+                    <p>Clínica Javier Prado</p>
+                    <p>12/03/2020</p>
+                </div>
+            </div>
+        </div>
+        <div class="pedia">
+            <h3>Pediatria 2</h3>
+            <div class="inf">
+                <div class="ima">
+                    <img src="../recursos/imagenes/pediatra2.jpg" alt="">
+                </div>
+                <div class="infor">
+                    <p>Nombre y apellido</p>
+                    <p>CMP:</p>
+                    <p>Lugar de atención</p>
+                    <p>Fecha de última atención</p>
+                </div>
+                <div class="infor">
+                    <p>Lucia Chavez Quispe</p>
+                    <p>725638</p>
+                    <p>POsta San Juan Miraflores</p>
+                    <p>02/01/2021</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+<script type="text/javascript" src="../recursos/js/general.js?v=0.6"></script>
+<script type="text/javascript" src="../recursos/js/solicitud.js?v=0.9"></script>
+</html>
