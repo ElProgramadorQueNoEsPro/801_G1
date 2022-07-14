45e2797bc1020eff7e9744918a2e7f3aa2045278
ashleyTE <20183321@aloe.ulima.edu.pe>
2022-07-14 11:02:28 -0500

    Create regisvacun.php

---
+++ vista/regisvacun.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../recursos/css/centrovac.css">
+    <title>Registro de Vacunación</title>
+</head>
+<body>
+    <section>
+        <h1>Registrar Vacuna</h1>
+        <p>Fecha 21/12/21</p>
+        <p>Hora: 9:00 am</p>
+        <div class="qr">
+            <h3>Obtener datos del menor buscando por DNI</h3>
+            <input type="search" id="search" placeholder="dni..." />
+            <button class="icon"><i class="fa fa-search"></i></button>
+        </div>
+        <div class="datomen">
+            <h3>Datos del menor</h3>
+                <input type="search" id="search" placeholder="Nombres..." />
+                <input type="search" id="search" placeholder="Apellidos..." />
+                <button>AGREGAR</button>
+        </div>
+        <div class="datvac">
+            <h3>Datos de la Vacuna</h3>
+            <input type="search" id="search" placeholder="Vacuna..." />
+            <input type="search" id="search" placeholder="Local de vacunaciòn..." />
+            <input type="search" id="search" placeholder="Dosis..." />
+            <input type="search" id="search" placeholder="Nùmero de Lote..." />
+        </div>
+    </section>
+</body>
+</html>
