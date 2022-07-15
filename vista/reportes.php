83a6cf201627b79ed20d08749cb2b660e62148ee
ashleyTE <20183321@aloe.ulima.edu.pe>
2022-07-15 16:07:56 -0500

    Create reportes.php

---
+++ vista/reportes.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../recursos/css/reportes.css">
+    <title>Reportes</title>
+</head>
+<body>
+    <section>
+        <h3>Reporte a la fecha</h3>
+        <table class="demo">
+            <thead>
+            <tr>
+                <th>Fecha</th>
+                <th>Vacuna</th>
+                <th>Dosis</th>
+                <th>DNI</th>
+                <th>Apellidos</th>
+                <th>Nombres</th>
+                <th>Lugar de Vacunación</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>25/07/2019</td>
+                <td>Hepatitis B</td>
+                <td>1ra Entrega</td>
+                <td>58632145</td>
+                <td>Bendezu Espinoza</td>
+                <td>Cesar Augusto</td>
+                <td>Tarma</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tbody>
+        </table>
+    </section>
+</body>
+</html>
