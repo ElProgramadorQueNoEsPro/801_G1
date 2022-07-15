eef1153724f1e53cf2ed449cdfc5a9a971bdf4a4
ashleyTE <20183321@aloe.ulima.edu.pe>
2022-07-15 16:06:15 -0500

    Create qr.php

---
+++ vista/qr.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../recursos/css/qr.css">
+    <title>QR</title>
+</head>
+<body>
+    <section>
+       <h1>Escanear QR para visualizar informaciòn</h1>
+       <img src="../recursos/imagenes/qr.png" alt="">
+       <p>Escanee el còdigo QR del niño usando su celular o el lector
+        de còdigo qr
+       </p>
+       <button>Iniciar</button>
+    </section>
+</body>
+</html>
