4680a8233bada0a6cae9ff801e04b926e8bc836d
ashleyTE <20183321@aloe.ulima.edu.pe>
2022-07-15 16:04:44 -0500

    Create consultaweb.php

---
+++ vista/consultaweb.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../recursos/css/consultaweb.css">
+    <title>Consulta Web</title>
+</head>
+<body>
+    <section>
+        <div class="box">
+            <div class="container-4">
+                <p>DNI</p>
+                <input type="search" id="search" placeholder="Buscar..." />
+                <button class="icon"><i class="fa fa-search"></i></button>
+            </div>
+        </div>
+        <div class="datos">
+            <h3>Datos del menor</h3>
+            <p>Matias Peña Sanchez</p>
+            <p>9 meses</p>
+            <p>9.2 kg</p>
+            <p>72 cm</p>
+        </div>
+        <div class="hist">
+            <h3>Historial de vacunas</h3>
+                <table class="demo">
+                    <thead>
+                    <tr>
+                        <th>Fecha</th>
+                        <th>Vacuna</th>
+                        <th>Dosis</th>
+                        <th>Local de Vacunación</th>
+                        <th>Vacunador</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>22/10/2020</td>
+                        <td>HEPATISIS B</td>
+                        <td>RN</td>
+                        <td>SURCO</td>
+                        <td>PATRICIA MONTES</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    </tbody>
+                </table>
+        </div>
+    </section>
+</body>
+</html>
